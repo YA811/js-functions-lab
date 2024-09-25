38c0d90b73c4104694b8615c93a69cae66f6a157
YA811 <yousifahmedd21@gmail.com>
2024-09-25 09:27:57 +0300

    home work is done

--- script.js
+++ script.js
@@ -237,7 +237,49 @@ else if(score >=70 && score<=79){
 else if(score >=60 && score<=69){
     return "D";
 }
+else{
+    return "F";
+}
 
 }
 
 console.log('Exercise 10 Result:', calculateGrade(85));
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre12'.
+
+Complete the exercise in the space below:
+*/
+
+function createUsername(firstName, lastName){
+
+    return firstName.slice(0,3)+lastName.slice(0,3)+(firstName.length+lastName.length);
+
+}
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs(...arguments){
+    return arguments.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4,));
